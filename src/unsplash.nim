e3e6af3cbb34ecd2476cb9cc886726de914a54a4
juancarlospaco <juancarlospaco@gmail.com>
2018-12-29 05:04:10 -0300

    Add App feature allows module to work as an CLI App

--- src/unsplash.nim
+++ src/unsplash.nim
@@ -19,7 +19,7 @@ template clientify(this: Unsplash | AsyncUnsplash): untyped =
       timeout = when declared(this.timeout): this.timeout.int * 1_000 else: -1,
       proxy = when declared(this.proxy): this.proxy else: nil, userAgent="")
 
-proc random*(this: Unsplash | AsyncUnsplash, width, height: int16): Future[string] {.multisync.} =
+proc randomPhoto*(this: Unsplash | AsyncUnsplash, width, height: int16): Future[string] {.multisync.} =
   ## Return a Random photo from a random user on unsplash.
   clientify(this)
   let url = unsplashApiUrl & "random/" & $width & "x" & $height
@@ -102,7 +102,7 @@ proc getPhoto*(this: Unsplash | AsyncUnsplash, photoId: string, width, height: i
 
 when isMainModule and not defined(release):
   let cliente = Unsplash(timeout: 99)
-  echo cliente.random(800, 600)
+  echo cliente.randomPhoto(800, 600)
   echo cliente.randomFromUser("juancarlospaco", 800, 600)
   echo cliente.likesFromUser("juancarlospaco", 800, 600)
   echo cliente.randomFromCollection(139386, 800, 600)
@@ -112,3 +112,54 @@ when isMainModule and not defined(release):
   echo cliente.weeklyRandom(@["cats", "girls"], 800, 600)
   echo cliente.featuredRandom(@["cats", "girls"], 800, 600)
   echo cliente.getPhoto("DfKZs6DOrw4", 800, 600)
+else:
+  import parseopt, terminal, random, times
+  {.passL: "-s", passC: "-flto -ffast-math", optimization: size.}
+  var
+    user: string
+    debug: bool
+    quantity = 1.byte
+    taimaout = 99.byte
+    width = 1024.int16
+    height = 768.int16
+    search: seq[string]
+    filename = $now()
+  for tipoDeClave, clave, valor in getopt():
+    case tipoDeClave
+    of cmdShortOption, cmdLongOption:
+      case clave
+      of "version":              quit("0.1.0", 0)
+      of "license", "licencia":  quit("MIT", 0)
+      of "help", "ayuda":        quit("./unsplash --debug --color --width=800 --height=600 --timeout=99 --quantity=3 --filename=coolbackground cat,girl,landscape", 0)
+      of "timeout":              taimaout = valor.parseInt.byte
+      of "quantity", "cantidad": quantity = valor.parseInt.byte
+      of "user", "usuario":      user = valor.string.normalize
+      of "filename", "archivo":  filename = valor.string.normalize
+      of "width", "ancho":       width = valor.parseInt.int16
+      of "height", "alto":       height = valor.parseInt.int16
+      of "debug", "desbichar":   debug = true
+      of "color":
+        randomize()
+        setBackgroundColor(bgBlack)
+        setForegroundColor([fgRed, fgGreen, fgYellow, fgBlue, fgMagenta, fgCyan, fgWhite].rand)
+    of cmdArgument:
+      search = clave.string.normalize.split(",")
+    of cmdEnd: quit("Wrong Parameters, see Help with --help", 1)
+  if filename.endsWith(".jpg"):
+    quit("Filename must Not include the file extension, Wrong Parameters, see Help with --help", 1)
+  let clientito = Unsplash(timeout: taimaout)
+  if user != "" and search != @[""]:
+    for i in 0.byte..quantity:
+      writeFile(filename & $i & ".jpg", clientito.weeklyFromUser(user, search, width, height))
+  elif user != "" and search == @[""]:
+    for i in 0.byte..quantity:
+      writeFile(filename & $i & ".jpg", clientito.randomFromUser(user, width, height))
+  elif user == "" and search != @[""]:
+    for i in 0.byte..quantity:
+      writeFile(filename & $i & ".jpg", clientito.featuredRandom(search, width, height))
+  elif user == "" and search == @[""]:
+    for i in 0.byte..quantity:
+      writeFile(filename & $i & ".jpg", clientito.randomPhoto(width, height))
+  if unlikely(debug):
+    echo "\nUSER\t", user, "\nQUERY\t", search, "\nWIDTH\t", width, "\nHEIGHT\t", height
+    echo "TIMEOUT\t", taimaout, "\nFILE\t", filename, "\nREPEAT\t", quantity
